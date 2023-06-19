916a5110ddb1caa2be34f0ae9d09343059abe4eb
Zerojav <javiermero8@gmail.com>
2023-06-19 02:02:03 -0500

    acercade y registrodoc

--- src/app/registrodoc/registrodoc.component.ts
+++ src/app/registrodoc/registrodoc.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class RegistrodocComponent {
 
+  constructor() { }
+
+  registrodoc() {
+    const pnombre = (<HTMLInputElement>document.getElementById("pnombre")).value;
+    const facultad = (<HTMLInputElement>document.getElementById("facultad")).value;
+    const correo = (<HTMLInputElement>document.getElementById("correo")).value;
+    const telefono = (<HTMLInputElement>document.getElementById("telefono")).value;
+    const asignatura = (<HTMLInputElement>document.getElementById("asignatura")).value;
+    const horaentrada = (<HTMLInputElement>document.getElementById("horaentrada")).value;
+    const horasalida = (<HTMLInputElement>document.getElementById("horasalida")).value;
+
+    if (pnombre === "" || facultad === "" || correo === "" || telefono === "" ||
+     asignatura === "" || horaentrada === "" || horasalida === "") {
+      alert("Por favor complete todos los campos requeridos.");
+    } else {
+      alert("Datos enviados");
+      location.reload();
+    }
+  }
 }
+
