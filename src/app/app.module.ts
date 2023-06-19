cc7022e249f1de20c79662b9ab5bff84d41b4bc1
Zerojav <javiermero8@gmail.com>
2023-06-18 22:01:05 -0500

    confirm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { IndexComponent } from './index/index.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuario: string = '';
+  contrasena: string = '';
 
+  constructor(private router: Router) {}
+
+  iniciarSesion() {
+    if (
+      (this.usuario === 'admin' && this.contrasena === 'admin') ||
+      (this.usuario === 'Javier' && this.contrasena === '1234')
+    ) {
+      // Iniciar sesión exitosamente
+      localStorage.setItem('loggedIn', 'true');
+      this.router.navigate(['index']);
+    } else {
+      // Mostrar mensaje de error
+      alert('Credenciales incorrectas');
+      location.reload();
+    }
+  }
 }
