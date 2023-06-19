246d920f302596c3cfc2247bdee34a4aab945bca
Zerojav <javiermero8@gmail.com>
2023-06-19 01:25:35 -0500

    asistencia

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -9,6 +9,51 @@ export class InicioComponent {
   mensaje() {
     alert("Asistencia Registrada");
     location.reload();
+  }
+  importarXML() {
+    $.ajax({
+      type: 'GET',
+      url: '../../resources/profesores.xml',
+      dataType: 'xml'
+    }).done((data) => {
+      $('#tabla-asistencia tbody').empty();
+      $(data).find('profesor').each(function() {
+        let fila = $('<tr>');
+        fila.append($(`<td>${$(this).find('nombre').text()}</td>`)),
+        fila.append($(`<td>${$(this).find('facultad').text()}</td>`)),
+        fila.append($(`<td>${$(this).find('asignatura').text()}</td>`));
+        fila.append($(`<td>${$(this).find('correo').text()}</td>`));
+        fila.append($(`<td>${$(this).find('telefono').text()}</td>`));
+        fila.append($(`<td>${$(this).find('horadentradaysalida').text()}</td>`));
+        let tdAsistencia = $('<td>');
+        let checkbox = $('<input type="checkbox">');
+        tdAsistencia.append(checkbox);
+        fila.append(tdAsistencia);
+        $('#tabla-asistencia tbody').append(fila);
+      });
+    });
+  }
+
+  importarJSON() {
+    $.ajax({
+      type: 'GET',
+      url: '../../resources/profesores.json',
+      dataType: 'json'
+    }).done((data) => {
+      $('#tabla-asistencia tbody').empty();
+      $.each(data, function(indice, profesor) {
+        let fila = $('<tr>');
+        fila.append($(`<td>${profesor.nombre}</td>`));
+        fila.append($(`<td>${profesor.facultad}</td>`));
+        fila.append($(`<td>${profesor.asignatura}</td>`));
+        fila.append($(`<td>${profesor.correo}</td>`));
+        fila.append($(`<td>${profesor.telefono}</td>`));
+        fila.append($(`<td>${profesor.horaEyS}</td>`));
+        let checkbox = $('<input>').attr('type', 'checkbox').attr('name', 'asistencia');
+        fila.append($('<td>').append(checkbox));
+        $('#tabla-asistencia tbody').append(fila);
+      });
+    });
   }
 
 }
