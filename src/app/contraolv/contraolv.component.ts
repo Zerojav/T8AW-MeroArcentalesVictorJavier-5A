3be993406e46f2ab560cf98634a9b8f945b5777d
Zerojav <javiermero8@gmail.com>
2023-06-19 03:06:15 -0500

    a

--- src/app/contraolv/contraolv.component.ts
+++ src/app/contraolv/contraolv.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./contraolv.component.css']
 })
 export class ContraolvComponent {
-
+  contraolv() {
+    alert("Peticion enviada, revise su correo");
+    location.reload();
+  }
 }
